a2bf3f26259cb6b26b31c99d4b5f174632fac86a
John James Gutib <jjgutib@gmail.com>
2023-09-18 21:25:21 +0800

    UI layout work

--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var custom_res_text_box : LineEdit
+@export var custom_res: Array[Control]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_res_selected(index):
+	if index == 5:
+		for control in custom_res:
+			control.visible = true
+	else:
+		for control in custom_res:
+			control.visible = false
 
 func _on_custom_res_input(input_string):
 	custom_res_text_box.release_focus()
