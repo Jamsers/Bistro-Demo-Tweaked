e9e1efd1f1b8013222e4c2db086ae6f17c410d24
John James Gutib <jjgutib@gmail.com>
2023-10-16 17:05:53 +0800

    Allow setting of initial rotation

--- Godot-Human-For-Scale/Assets/HumanController.gd
+++ Godot-Human-For-Scale/Assets/HumanController.gd
@@ -21,16 +21,17 @@ const TP_CAMERA_DISTANCE = 2.1
 const TRANSITION_SPEED = 0.25
 const LOOK_LIMIT_UPPER = 1.25
 const LOOK_LIMIT_LOWER = -1.25
-const ANIM_MOVE_SPEED = 3
+const ANIM_MOVE_SPEED = 3.0
 const ANIM_RUN_SPEED = 5.5
-const NOCLIP_MULT = 4
+const ANIM_BLEND_TIME = 0.2
+const NOCLIP_MULT = 4.0
 const ROTATE_SPEED = 12.0
 const JUMP_FORCE = 15.0
 const GRAVITY_FORCE = 50.0
 const COLLIDE_FORCE = 0.05
 const DIRECTIONAL_FORCE_DIV = 30.0
 const TOGGLE_COOLDOWN = 0.5
-const DOF_MOVE_SPEED = 40
+const DOF_MOVE_SPEED = 40.0
 const DOF_INTENSITY = 0.25
 
 var move_direction = Vector3.ZERO
@@ -47,9 +48,9 @@ var shoulder_is_swapped = false
 var shoulder_cooldown = 0.0
 var mousecapture_on = true
 var mousecapture_toggle_cooldown = 0.0
-var rigidbody_collisions = []
-var input_velocity = Vector3.ZERO
 var is_cam_transitioning = false
+var input_velocity = Vector3.ZERO
+var rigidbody_collisions = []
 
 var mouse_movement = Vector2.ZERO
 var forward_isdown = false
@@ -64,57 +65,35 @@ var mousecapture_isdown = false
 var zoom_isdown = false
 var shoulder_isdown = false
 
+@onready var model_root = $"ModelRoot"
+@onready var anim_player = $"ModelRoot/mannequiny-0_3_0/AnimationPlayer"
+@onready var player_mesh = $"ModelRoot/mannequiny-0_3_0/root/Skeleton3D/mannequiny"
+@onready var collision_shape = $"CollisionShape"
 @onready var camera_pivot = $"CameraPivot"
 @onready var spring_arm = $"CameraPivot/SpringArm"
 @onready var camera = $"CameraPivot/SpringArm/Camera"
 @onready var focus_ray = $"CameraPivot/SpringArm/Camera/RayCast3D"
-@onready var player_mesh = $"ModelRoot/mannequiny-0_3_0/root/Skeleton3D/mannequiny"
-@onready var anim_player = $"ModelRoot/mannequiny-0_3_0/AnimationPlayer"
 
 func _ready():
-	basis = Basis.IDENTITY
-	anim_player.playback_default_blend_time = 0.2
+	var y_rotation = Vector3(0.0, global_rotation.y, 0.0)
+	
+	global_rotation = Vector3.ZERO
+	
+	camera_pivot.global_rotation = y_rotation
+	model_root.global_rotation = y_rotation
+	camera_rotation = Quaternion.from_euler(camera_pivot.global_rotation)
+	camera_rotation_no_y = Quaternion.from_euler(camera_pivot.global_rotation)
+	
+	anim_player.playback_default_blend_time = ANIM_BLEND_TIME
+	
 	if enable_depth_of_field:
 		hijack_camera_attributes()
 
-func hijack_camera_attributes():
-	var cams_or_envs = []
-	cams_or_envs += get_tree().current_scene.find_children("*", "WorldEnvironment", true)
-	cams_or_envs += get_tree().current_scene.find_children("*", "Camera3D", true)
-	var hijacked_attributes = null
-	
-	for node in cams_or_envs:
-		if node is WorldEnvironment:
-			if node.camera_attributes != null:
-				hijacked_attributes = node.camera_attributes
-				break
-		if node is Camera3D:
-			if node.attributes != null:
-				hijacked_attributes = node.attributes
-				break
-	
-	camera.attributes = create_practical_attributes(hijacked_attributes)
-
-func create_practical_attributes(attributes):
-	var practical_attributes = CameraAttributesPractical.new()
-	
-	if attributes != null:
-		if attributes is CameraAttributesPractical:
-			practical_attributes = attributes.duplicate()
-		else:
-			practical_attributes.auto_exposure_enabled = attributes.auto_exposure_enabled
-			practical_attributes.auto_exposure_scale = attributes.auto_exposure_scale
-			practical_attributes.auto_exposure_speed = attributes.auto_exposure_speed
-			practical_attributes.exposure_multiplier = attributes.exposure_multiplier
-			practical_attributes.exposure_sensitivity = attributes.exposure_sensitivity
-	
-	return practical_attributes
-
 func _process(delta):
-	process_mousecapture(delta)
 	process_camera()
 	process_movement()
 	process_animation(delta)
+	process_mousecapture(delta)
 	process_noclip(delta)
 	process_cam_toggle(delta)
 	process_cam_zoom(delta)
@@ -156,29 +135,16 @@ func _physics_process(delta):
 		collision.get_collider().apply_central_impulse(direction * central_multiplier)
 		collision.get_collider().apply_impulse(direction * directional_multiplier, location)
 
-func process_mousecapture(delta):
-	if mousecapture_isdown and mousecapture_toggle_cooldown == 0:
-		mousecapture_on = !mousecapture_on
-		mousecapture_toggle_cooldown = TOGGLE_COOLDOWN
-	
-	if mousecapture_on:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	else:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	mousecapture_toggle_cooldown -= delta
-	mousecapture_toggle_cooldown = clamp(mousecapture_toggle_cooldown, 0, TOGGLE_COOLDOWN)
-
 func process_camera():
 	var camera_rotation_euler = camera_rotation.get_euler()
 	
 	if mousecapture_on:
-		camera_rotation_euler += Vector3(mouse_movement.y, mouse_movement.x, 0) * LOOK_SENSITIVITY
+		camera_rotation_euler += Vector3(mouse_movement.y, mouse_movement.x, 0.0) * LOOK_SENSITIVITY
 		camera_rotation_euler.x = clamp(camera_rotation_euler.x, LOOK_LIMIT_LOWER, LOOK_LIMIT_UPPER)
 	
 	camera_rotation = Quaternion.from_euler(camera_rotation_euler)
-	$CameraPivot.basis = Basis(camera_rotation)
-	camera_rotation_no_y = Basis($CameraPivot.basis.x, Vector3.UP, $CameraPivot.basis.z).get_rotation_quaternion()
+	camera_pivot.global_basis = Basis(camera_rotation)
+	camera_rotation_no_y = Basis(camera_pivot.global_basis.x, Vector3.UP, camera_pivot.global_basis.z).get_rotation_quaternion()
 	
 	mouse_movement = Vector2.ZERO
 
@@ -211,43 +177,56 @@ func process_animation(delta):
 		switch_anim("Idle")
 	
 	if move_direction != Vector3.ZERO:
-		$ModelRoot.basis = basis_rotate_toward($ModelRoot.basis, Basis.looking_at(move_direction_no_y), ROTATE_SPEED * delta)
+		model_root.global_basis = basis_rotate_toward(model_root.global_basis, Basis.looking_at(move_direction_no_y), ROTATE_SPEED * delta)
+
+func process_mousecapture(delta):
+	if mousecapture_isdown and mousecapture_toggle_cooldown == 0.0:
+		mousecapture_on = !mousecapture_on
+		mousecapture_toggle_cooldown = TOGGLE_COOLDOWN
+	
+	if mousecapture_on:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	mousecapture_toggle_cooldown -= delta
+	mousecapture_toggle_cooldown = clamp(mousecapture_toggle_cooldown, 0.0, TOGGLE_COOLDOWN)
 
 func process_noclip(delta):
-	if noclip_isdown and noclip_toggle_cooldown == 0:
+	if noclip_isdown and noclip_toggle_cooldown == 0.0:
 		noclip_on = !noclip_on
-		$CollisionShape.disabled = !$CollisionShape.disabled
+		collision_shape.disabled = !collision_shape.disabled
 		noclip_toggle_cooldown = TOGGLE_COOLDOWN
 	
 	noclip_toggle_cooldown -= delta
-	noclip_toggle_cooldown = clamp(noclip_toggle_cooldown, 0, TOGGLE_COOLDOWN)
+	noclip_toggle_cooldown = clamp(noclip_toggle_cooldown, 0.0, TOGGLE_COOLDOWN)
 
 func process_cam_toggle(delta):
-	if cam_toggle_isdown and cam_toggle_cooldown == 0 and !is_cam_transitioning:
+	if cam_toggle_isdown and cam_toggle_cooldown == 0.0 and !is_cam_transitioning:
 		cam_is_fp = !cam_is_fp
 		cam_toggle_cooldown = TOGGLE_COOLDOWN
 		cam_transition()
 	
 	cam_toggle_cooldown -= delta
-	cam_toggle_cooldown = clamp(cam_toggle_cooldown, 0, TOGGLE_COOLDOWN)
+	cam_toggle_cooldown = clamp(cam_toggle_cooldown, 0.0, TOGGLE_COOLDOWN)
 
 func process_cam_zoom(delta):
-	if zoom_isdown and cam_zoom_cooldown == 0 and !is_cam_transitioning:
+	if zoom_isdown and cam_zoom_cooldown == 0.0 and !is_cam_transitioning:
 		cam_is_zoomed = !cam_is_zoomed
 		cam_zoom_cooldown = TOGGLE_COOLDOWN
 		cam_transition()
 	
 	cam_zoom_cooldown -= delta
-	cam_zoom_cooldown = clamp(cam_zoom_cooldown, 0, TOGGLE_COOLDOWN)
+	cam_zoom_cooldown = clamp(cam_zoom_cooldown, 0.0, TOGGLE_COOLDOWN)
 
 func process_shoulder_swap(delta):
-	if shoulder_isdown and shoulder_cooldown == 0 and !is_cam_transitioning and !cam_is_fp:
+	if shoulder_isdown and !cam_is_fp and shoulder_cooldown == 0.0 and !is_cam_transitioning:
 		shoulder_is_swapped = !shoulder_is_swapped
 		shoulder_cooldown = TOGGLE_COOLDOWN
 		cam_transition()
 	
 	shoulder_cooldown -= delta
-	shoulder_cooldown = clamp(shoulder_cooldown, 0, TOGGLE_COOLDOWN)
+	shoulder_cooldown = clamp(shoulder_cooldown, 0.0, TOGGLE_COOLDOWN)
 
 func cam_transition():
 	if is_cam_transitioning:
@@ -296,7 +275,7 @@ func cam_transitioning(height, offset, length, fov, mesh_visible):
 		var current_time = Time.get_ticks_msec()
 		var lerp = (current_time - time)/(TRANSITION_SPEED * 1000.0)
 		
-		if lerp > 1:
+		if lerp >= 1.0:
 			camera_pivot.position.y = height
 			spring_arm.position.x = offset
 			spring_arm.spring_length = length
@@ -350,7 +329,40 @@ func process_dof(delta):
 	camera.attributes.dof_blur_near_transition = move_toward(camera.attributes.dof_blur_near_transition, near_transition, delta * DOF_MOVE_SPEED) 
 	camera.attributes.dof_blur_far_distance =  move_toward(camera.attributes.dof_blur_far_distance, far_distance, delta * DOF_MOVE_SPEED)
 	camera.attributes.dof_blur_far_transition = move_toward(camera.attributes.dof_blur_far_transition, far_transition, delta * DOF_MOVE_SPEED)
-	camera.attributes.dof_blur_amount = move_toward(camera.attributes.dof_blur_amount, blur_amount, delta * (DOF_MOVE_SPEED/10))
+	camera.attributes.dof_blur_amount = move_toward(camera.attributes.dof_blur_amount, blur_amount, delta * (DOF_MOVE_SPEED/10.0))
+
+func hijack_camera_attributes():
+	var cams_or_envs = []
+	cams_or_envs += get_tree().current_scene.find_children("*", "Camera3D", true)
+	cams_or_envs += get_tree().current_scene.find_children("*", "WorldEnvironment", true)
+	var hijacked_attributes = null
+	
+	for node in cams_or_envs:
+		if node is Camera3D:
+			if node.attributes != null:
+				hijacked_attributes = node.attributes
+				break
+		if node is WorldEnvironment:
+			if node.camera_attributes != null:
+				hijacked_attributes = node.camera_attributes
+				break
+	
+	camera.attributes = create_practical_attributes(hijacked_attributes)
+
+func create_practical_attributes(attributes):
+	var practical_attributes = CameraAttributesPractical.new()
+	
+	if attributes != null:
+		if attributes is CameraAttributesPractical:
+			practical_attributes = attributes.duplicate()
+		else:
+			practical_attributes.auto_exposure_enabled = attributes.auto_exposure_enabled
+			practical_attributes.auto_exposure_scale = attributes.auto_exposure_scale
+			practical_attributes.auto_exposure_speed = attributes.auto_exposure_speed
+			practical_attributes.exposure_multiplier = attributes.exposure_multiplier
+			practical_attributes.exposure_sensitivity = attributes.exposure_sensitivity
+	
+	return practical_attributes
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -384,15 +396,15 @@ func _unhandled_input(event):
 			KEY_TAB:
 				shoulder_isdown = event.pressed
 
-func switch_anim(anim, speed = 1):
+func switch_anim(anim, speed = 1.0):
 	if anim_player.current_animation != anim:
 		anim_player.play(anim, -1, speed)
 
-static func quat_rotate_toward(from: Quaternion, to: Quaternion, delta: float) -> Quaternion:
+func quat_rotate_toward(from: Quaternion, to: Quaternion, delta: float) -> Quaternion:
 	return from.slerp(to, clamp(delta / from.angle_to(to), 0.0, 1.0)).normalized()
 
-static func basis_rotate_toward(from: Basis, to: Basis, delta: float) -> Basis:
+func basis_rotate_toward(from: Basis, to: Basis, delta: float) -> Basis:
 	return Basis(quat_rotate_toward(from.get_rotation_quaternion(), to.get_rotation_quaternion(), delta)).orthonormalized()
 
-func ease_in_out_sine(lerp):
-	return -(cos(PI * lerp) - 1) / 2;
+func ease_in_out_sine(lerp: float) -> float:
+	return -(cos(PI * lerp) - 1.0) / 2.0;
