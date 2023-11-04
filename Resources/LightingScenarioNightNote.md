a0e058ed89dde91dbc448fedd51eacd6489f76cd
John James Gutib <jjgutib@gmail.com>
2023-11-05 04:43:00 +0800

    Transfer note about night values

---
+++ Resources/LightingScenarioNightNote.md
@@ -0,0 +1,3 @@
+For night time, we use non-realistic "filmic" values - brightness values are 10 times higher than the reference values.
+Reference values for night time are 0.3 lux for the moon with a 4000k temp, and 0.09 nits for the sky. It goes much lower than that when it isn't a full moon, or if it's a cloudy night.
+Godot can handle those values just fine... it's just that they don't look very good.
--- Scripts/Lighting.gd
+++ Scripts/Lighting.gd
@@ -2,10 +2,6 @@ extends Resource
 
 class_name Lighting
 
-# For night time, we use non realistic "filmic" values.
-# Real night time values are 0.3 lux for the moon with a 4000k temp, and 0.09 nits for the sky.
-# Godot can handle those values just fine... it's just that they don't look very good.
-
 @export var rotation: Quaternion = Quaternion.IDENTITY
 @export var temp: float = 0.0
 @export var lux: float = 0.0
