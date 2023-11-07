a06c8b1e279aaf90edbbc63daf5aa46543d2a065
John James Gutib <jjgutib@gmail.com>
2023-11-07 17:42:50 +0800

    Scraping code done

--- Scripts/PhysicsPropSoundPlayer.gd
+++ Scripts/PhysicsPropSoundPlayer.gd
@@ -1,22 +1,40 @@
 extends RigidBody3D
 
 @export var phys_sound_player: AudioStreamPlayer3D
+@export var scrape_sound_player: AudioStreamPlayer3D
 
 @export var prop_sounds: Array[AudioStreamWAV]
 
 var prop_sounds_loaded = []
 
 var on_cooldown = false
+var scraping_on_cooldown = false
 
 func _ready():
 	for resource in prop_sounds:
 		prop_sounds_loaded.append(load(resource.resource_path))
+	scrape_sound_player.stream_paused = true
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
-	pass
+	var is_scraping = false
+	
+	if get_contact_count() > 0:
+		if linear_velocity.length() > 2.0:
+			#higher speed, higher volume
+			is_scraping = true
+	
+	set_scraping_pause(is_scraping)
+
+func set_scraping_pause(play):
+	if scraping_on_cooldown:
+		return
+	scraping_on_cooldown = true
+	scrape_sound_player.stream_paused = !play
+	await get_tree().create_timer(0.15).timeout
+	scraping_on_cooldown = false
 
 func _integrate_forces(state):
 	if on_cooldown:
@@ -29,7 +47,7 @@ func _integrate_forces(state):
 			on_cooldown = true
 			phys_sound_player.stream = prop_sounds_loaded.pick_random()
 			phys_sound_player.play()
-			await get_tree().create_timer(0.1).timeout
+			await get_tree().create_timer(0.15).timeout
 			on_cooldown = false
 			break
 
